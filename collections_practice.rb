b6ddfd28fae5dfc0ea9464955d96b7332565c883
Victoria Meng <nights1ststar@protonmail.com>
2018-03-10 01:55:18 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -17,7 +17,7 @@ def swap_elements(integers)
   integers[1] = third
   integers
 
-  array[1], array[2] = array[2], array[1]
+  integers[1], integers[2] = integers[2], integers[1]
 end
 
 def reverse_array(integers)
