d327be63a9f91028984acb6b90d8f4a3b6078942
Victoria Meng <nights1ststar@protonmail.com>
2018-03-10 02:02:20 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -12,11 +12,6 @@ def sort_array_char_count(integers)
 end
 
 def swap_elements(integers)
-  # third = integers[2]
-  # integers[2] = integers[1]
-  # integers[1] = third
-  # integers
-
   integers[1], integers[2] = integers[2], integers[1]
   integers
 end
